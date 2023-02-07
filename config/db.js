15162f48df2c19dd63978b080833d82fc1160131
UdaySamsani <satyasaisamsani@gmail.com>
2023-02-07 21:13:26 +0530

    intiliasing db

--- config/db.js
+++ config/db.js
@@ -1,4 +1,17 @@
 const mongoose = require('mongoose');
 
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MongoUri, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+            useUnifiedTopology: true,
+        });
+        console.log('Datbase connected');
+    } catch (err) {
+        console.log(err);
+    }
+};
 
 module.exports = connectDB;
